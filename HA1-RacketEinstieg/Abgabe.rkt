1b1735b3f18d452485735e4ba5d76c7f244c9682
Paul Wagner <paulkonstantin.wagner@stud.tu-darmstadt.de>
2016-11-02 18:12:58 +0100

    Abgabe I (v0.1)

---
+++ HA1-RacketEinstieg/Abgabe.rkt
@@ -0,0 +1,38 @@
+#lang racket
+
+;; TODOs
+;; * Vertraege
+;; * Tests
+;; * Korrekter Header/definierte sprache
+;; * Bisschen schoenere Struktur (insbesondere saving-plans)
+;; * Dynamischer (braucht man das ueberhaupt?)
+;; * TODOs in code...
+
+;; 9.1
+(define (add-interest capital interestRate) 
+	(* capital (+ interestRate 1)))
+
+;; 9.2
+(define (average-yearly-return capitalBefore capitalAfter duration) 
+	(expt (- (/ capitalAfter capitalBefore) 1) (/ 1 duration)))
+
+;; 9.4
+(define (best-savings-plan capital duration)
+	(if (>= (average-yearly-return capital (savings-plan-a capital duration) duration) (average-yearly-return capital (savings-plan-b capital duration) duration)) 'SavingsPlanA 'SavingsPlanB))
+
+
+;; 9.3
+;; TODO round (floor) duration
+(define (savings-plan-a capital duration)
+	(if (= duration 0) capital (savings-plan-a (add-interest capital (/
+		(cond	[(= duration 1) (/ 1 2)]
+                        [(= duration 2) 1]
+                        [(= duration 3) (/ 3 2)]) 100))
+	(- duration 1))))
+
+;; TODO round (floor) duration
+(define (savings-plan-b capital duration)
+	(if (= duration 0) capital (savings-plan-b (+ (add-interest capital (/ (/ 1 2) 100)) (if (= duration 3) 30 0)) (- duration 1))))
+
+;; TODO delete line in final version
+(best-savings-plan 10000 3)

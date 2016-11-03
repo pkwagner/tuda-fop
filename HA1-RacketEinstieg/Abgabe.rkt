0b2d4699d9cde6e1f5f13e53118f26fe078c0897
games647 <games@tsprivat.de>
2016-11-03 20:00:25 +0100

    Do more line breaks for a better overview

--- HA1-RacketEinstieg/Abgabe.rkt
+++ HA1-RacketEinstieg/Abgabe.rkt
@@ -2,7 +2,6 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname Abgabe) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 
-;; TEST
 ;; TODOs
 ;; * Vertraege
 ;; * Tests
@@ -21,8 +20,10 @@
 
 ;; 9.4
 (define (best-savings-plan capital duration)
-	(if (>= (average-yearly-return capital (savings-plan-a capital duration) duration) (average-yearly-return capital (savings-plan-b capital duration) duration)) 'SavingsPlanA 'SavingsPlanB))
-
+	(if (>= (average-yearly-return capital (savings-plan-a capital duration) duration)
+                (average-yearly-return capital (savings-plan-b capital duration) duration))
+            'SavingsPlanA
+            'SavingsPlanB))
 
 ;; 9.3
 ;; TODO round (floor) duration
@@ -35,7 +36,6 @@
 
 ;; TODO round (floor) duration
 (define (savings-plan-b capital duration)
-	(if (= duration 0) capital (savings-plan-b (+ (add-interest capital (/ (/ 1 2) 100)) (if (= duration 3) 30 0)) (- duration 1))))
-
-;; TODO delete line in final version
-(best-savings-plan 10000 3)
+	(if (= duration 0)
+            capital
+            (savings-plan-b (+ (add-interest capital (/ (/ 1 2) 100)) (if (= duration 3) 30 0)) (- duration 1))))

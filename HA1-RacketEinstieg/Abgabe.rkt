0d9ee4f57edaf58a4632bde9313a25ac31874b84
games647 <games@tsprivat.de>
2016-11-03 20:18:04 +0100

    Define constants to prevent magic values

--- HA1-RacketEinstieg/Abgabe.rkt
+++ HA1-RacketEinstieg/Abgabe.rkt
@@ -1,6 +1,9 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname Abgabe) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Constants
+(define tolerance 0.001)
+(define base-interest 0.005)
 
 ;; TODOs
 ;; * Vertraege
@@ -35,8 +38,48 @@
 	(expt (- (/ capitalAfter capitalBefore) 1) (/ 1 duration)))
 
 ;; Tests
-(check-within (average-yearly-return 100 101 1) 0.01 0.001)
-(check-within (average-yearly-return 100 225 2) 0.5 0.001)
+(check-within (average-yearly-return 100 101 1) 0.01 tolerance)
+(check-within (average-yearly-return 100 225 2) 0.5 tolerance)
+
+;; 9.3
+;; savings-plan-a::
+;;
+;; 
+;; 
+;; Ex:
+;; TODO round (floor) duration
+(define (savings-plan-a capital duration)
+	(if (= duration 0) capital (savings-plan-a (add-interest capital 
+		(cond	[(= duration 1) base-interest]
+                        [(= duration 2) (* base-interest 2)]
+                        [(= duration 3) (* base-interest 3)]))
+	(- duration 1))))
+
+;; Tests
+(check-error (savings-plan-a 100 0) "invalid runtime")
+(check-error (savings-plan-a 100 4) "invalid runtime")
+(check-expect (savings-plan-a 100 1) 105)
+(check-expect (savings-plan-a 100 1.5) 105)
+;; Includes the bonus
+(check-within (savings-plan-a 100 3) 133.027 tolerance)
+
+;; TODO round (floor) duration
+;; savings-plan-b::
+;;
+;; 
+;; 
+;; Ex:
+(define (savings-plan-b capital duration)
+	(if (= duration 0)
+            capital
+            (savings-plan-b (+ (add-interest capital base-interest (if (= duration 3) 30 0)) (- duration 1))))
+
+;; Tests
+(check-error (savings-plan-b 100 0) "invalid runtime")
+(check-error (savings-plan-b 100 4) "invalid runtime")
+(check-expect (savings-plan-b 100 1) 100.5)
+(check-expect (savings-plan-b 100 1.5) 100.5)
+(check-within (savings-plan-b 100 3) 101.507 tolerance)
 
 ;; 9.4
 ;; best-savings-plan::
@@ -53,43 +96,3 @@
 ;; Tests
 (check-expect (best-savings-plan 100 1) 'SavingsPlanA)
 (check-expect (best-savings-plan 100 3) 'SavingsPlanB)
-
-;; 9.3
-;; savings-plan-a::
-;;
-;; 
-;; 
-;; Ex:
-;; TODO round (floor) duration
-(define (savings-plan-a capital duration)
-	(if (= duration 0) capital (savings-plan-a (add-interest capital (/
-		(cond	[(= duration 1) (/ 1 2)]
-                        [(= duration 2) 1]
-                        [(= duration 3) (/ 3 2)]) 100))
-	(- duration 1))))
-
-;; Tests
-(check-error (savings-plan-a 100 0) "invalid runtime")
-(check-error (savings-plan-a 100 4) "invalid runtime")
-(check-expect (savings-plan-a 100 1) 105)
-(check-expect (savings-plan-a 100 1.5) 105)
-;; Includes the bonus
-(check-within (savings-plan-a 100 3) 133.027 0.001)
-
-;; TODO round (floor) duration
-;; savings-plan-b::
-;;
-;; 
-;; 
-;; Ex:
-(define (savings-plan-b capital duration)
-	(if (= duration 0)
-            capital
-            (savings-plan-b (+ (add-interest capital (/ (/ 1 2) 100)) (if (= duration 3) 30 0)) (- duration 1))))
-
-;; Tests
-(check-error (savings-plan-b 100 0) "invalid runtime")
-(check-error (savings-plan-b 100 4) "invalid runtime")
-(check-expect (savings-plan-b 100 1) 100.5)
-(check-expect (savings-plan-b 100 1.5) 100.5)
-(check-within (savings-plan-b 100 3) 101.507 0.001)

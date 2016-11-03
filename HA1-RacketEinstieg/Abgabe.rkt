7dc490c11bf266e058039492a5b7af436fb62583
games647 <games@tsprivat.de>
2016-11-03 20:04:39 +0100

    Add unit tests

--- HA1-RacketEinstieg/Abgabe.rkt
+++ HA1-RacketEinstieg/Abgabe.rkt
@@ -14,9 +14,19 @@
 (define (add-interest capital interestRate) 
 	(* capital (+ interestRate 1)))
 
+;; Tests
+(check-error (add-interest 100 -0.01) "no negative interest rates allowed")
+(check-expect (add-interest 100 0.01) 101)
+(check-expect (add-interest 0 0.01) 0)
+(check-expect (add-interest -1 0.01) -1)
+
 ;; 9.2
 (define (average-yearly-return capitalBefore capitalAfter duration) 
 	(expt (- (/ capitalAfter capitalBefore) 1) (/ 1 duration)))
+
+;; Tests
+(check-within (average-yearly-return 100 101 1) 1 0.001)
+(check-within (average-yearly-return 100 225 2) 50 0.001)
 
 ;; 9.4
 (define (best-savings-plan capital duration)
@@ -24,6 +34,10 @@
                 (average-yearly-return capital (savings-plan-b capital duration) duration))
             'SavingsPlanA
             'SavingsPlanB))
+
+;; Tests
+(check-expect (best-savings-plan 100 1) 'SavingsPlanA)
+(check-expect (best-savings-plan 100 3) 'SavingsPlanB)
 
 ;; 9.3
 ;; TODO round (floor) duration
@@ -34,8 +48,23 @@
                         [(= duration 3) (/ 3 2)]) 100))
 	(- duration 1))))
 
+;; Tests
+(check-error (savings-plan-a 100 0) "invalid runtime")
+(check-error (savings-plan-a 100 4) "invalid runtime")
+(check-expect (savings-plan-a 100 1) 105)
+(check-expect (savings-plan-a 100 1.5) 105)
+;; Includes the bonus
+(check-within (savings-plan-a 100 3) 133.027 0.001)
+
 ;; TODO round (floor) duration
 (define (savings-plan-b capital duration)
 	(if (= duration 0)
             capital
             (savings-plan-b (+ (add-interest capital (/ (/ 1 2) 100)) (if (= duration 3) 30 0)) (- duration 1))))
+
+;; Tests
+(check-error (savings-plan-b 100 0) "invalid runtime")
+(check-error (savings-plan-b 100 4) "invalid runtime")
+(check-expect (savings-plan-b 100 1) 100.5)
+(check-expect (savings-plan-b 100 1.5) 100.5)
+(check-within (savings-plan-b 100 3) 101.507 0.001)

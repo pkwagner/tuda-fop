9e895e38eab8454c80502571f0dfc9d0d947d003
games647 <games@tsprivat.de>
2016-11-04 12:05:34 +0100

    Add contracts, description and example

--- HA1-RacketEinstieg/Abgabe.rkt
+++ HA1-RacketEinstieg/Abgabe.rkt
@@ -1,29 +1,27 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata 
-;; about the language level of this file in a form that our tools can easily process. 
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname Abgabe) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; Constants
 (define tolerance 0.001)
 (define base-interest 0.005)
 
 ;; TODOs
-;; * Vertraege
-;; * Tests
-;; * Korrekter Header/definierte sprache
 ;; * Bisschen schoenere Struktur (insbesondere saving-plans)
 ;; * Dynamischer (braucht man das ueberhaupt? Ja)
 ;; * TODOs in code...
 
 ;; 9.1
 ;; add-interest:: number number -> number
-;; returns the new capital after adding the interest rate
+;; 
+;; Returns the new capital after adding the interest rate
+;; 
 ;; Example: (add-interest 10000 0.025) should return 10250
 (define (add-interest capital interestRate)
   (cond	[(< interestRate 0) (error "no negative interest rates allowed")]
         [(<= capital 0) capital ] 
         [(> capital 0) (* capital (+ interestRate 1))]
   )
-)
-	
+)	
 
 ;; Tests
 (check-error (add-interest 100 -0.01) "no negative interest rates allowed")
@@ -33,8 +31,11 @@
 
 ;; 9.2
 ;; average-yearly-return:: number number number -> number
-;; returns the average of yearly interest rates based 
-;; Ex: (average-yearly-return 3000 1000 1) should return 2
+;;
+;; Returns the average of yearly interest rates based on the start and end capital
+;; and the duration in years
+;;
+;; Ex: (average-yearly-return 3000 1000 1) = 2
 (define (average-yearly-return capitalBefore capitalAfter duration)
   (if (and (> duration 0) (> capitalBefore capitalAfter))
 	(expt (- (/ capitalBefore capitalAfter) 1) (/ 1 duration))
@@ -47,14 +48,16 @@
 (check-within (average-yearly-return 100 101 1) 0.01 tolerance)
 (check-within (average-yearly-return 100 225 2) 0.5 tolerance)
 
-(average-yearly-return 3000 2000 5)
-
 ;; 9.3
 ;; savings-plan-a:: number number -> number
-;; 
-;; 
-;; 
-;; Ex:
+;;
+;; Berechnet das Endkapital nach Ablauf einer Zeitperiode 
+;; laufzeit (für jahre) gemäß des Sparplans A 
+;; von einem gewissen Startkapital start-kapital
+;;
+;; Ex: 100€ Startkapital für 3 Jahre
+;;   (savings-plan-b 100 3) = 133.027 Endkapital
+
 ;; TODO round (floor) duration
 (define (savings-plan-a capital duration)
 	(if (= duration 0)
@@ -80,15 +83,23 @@
 (check-within (savings-plan-a 100 3) 133.027 tolerance)
 
 ;; TODO round (floor) duration
-;; savings-plan-b::
+
+;; savings-plan-b:: number number -> number
 ;;
+;; Berechnet das Endkapital nach Ablauf einer Zeitperiode 
+;; laufzeit (für jahre) gemäß des Sparplans B 
+;; von einem gewissen Startkapital start-kapital
 ;; 
-;; 
-;; Ex:
+;; Ex: 100€ Startkapital für 3 Jahre
+;;  (savings-plan-b 100 3) = 101.507 Endkapital
 (define (savings-plan-b capital duration)
   (if (= duration 0)
       capital
-      (savings-plan-b (+ (add-interest capital base-interest (if (= duration 3) 30 0)) (- duration 1))))
+      (savings-plan-b (+ (add-interest capital base-interest
+                                       (if (= duration 3)
+                                           30
+                                           0)
+                                       ) (- duration 1))))
   )
 
 ;; Tests
@@ -99,11 +110,13 @@
 (check-within (savings-plan-b 100 3) 101.507 tolerance)
 
 ;; 9.4
-;; best-savings-plan::
+;; best-savings-plan:: number number -> symbol
 ;;
+;; Returns the best saving plan for a certain start captial and a duration in years
+;;
+;; If the plans are identical the first one (A) will be returned
 ;; 
-;; 
-;; Ex:
+;; Ex: (best-savings-plan 100 3) = 'SavingsPlanB 
 (define (best-savings-plan capital duration)
 	(if (>= (average-yearly-return capital (savings-plan-a capital duration) duration)
                 (average-yearly-return capital (savings-plan-b capital duration) duration))

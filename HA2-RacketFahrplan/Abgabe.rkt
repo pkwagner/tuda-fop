0d3861e2eacec0d9623a6a3b16f23cc06c7d5e51
games647 <games@tsprivat.de>
2016-11-05 16:13:09 +0100

    Add basic header to the second task

--- HA2-RacketFahrplan/Abgabe.rkt
+++ HA2-RacketFahrplan/Abgabe.rkt
@@ -1,3 +1,9 @@
-#lang racket
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname Abgabe) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 
-;; PLACEHOLDER
+;; Authors:
+;; Alexander Siegler
+;; Paul Konstantin Wagner
+;; Yoshua Hitzel
+;; Marcel Lackovic

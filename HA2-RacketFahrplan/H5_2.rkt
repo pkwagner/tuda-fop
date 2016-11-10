8585f697e2f086dcbce9c0c38f86fcf8c89fb1b7
user1831 <muell@muell.muell>
2016-11-10 23:13:35 +0100

    5.2 implemented

--- HA2-RacketFahrplan/H5_2.rkt
+++ HA2-RacketFahrplan/H5_2.rkt
@@ -1,61 +1,72 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname H5_2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 
-;; Author*s:
+;; Author:
 ;; Yoshua Hitzel
 
+(define-struct station (name train-kinds distance-to-next))
 
-
-
-;; The following line is only for clarity reasons and should be removed later
-(require "_Template.rkt")
-
-;; Define Dummys used in previous tasks
-(define (find-stops stations train_kind) '(a-stadt c-stadt))
-
-
-
-;; ====== Problem 5.2 =======
-
-;; distance-table-offset: 
-;;
-;; 
-;; Example:
-;*
+;; distance-table-offset: (listof stations) number -> (listof stations)
+;; Adds the distances of every further stations to the following station
+;; Example: (distance-table (list
+;;            (make-station 'Waechtersbach '(RE SE) 4.5)
+;;            (make-station 'Wirtheim '(SE) 3)
+;;            (make-station 'HaitzHoechst '(SE) 3)
+;;          ) 'Waechtersbach
+;;        = (list (make-station 'Waechtersbach (list 'RE 'SE) 4.5) (make-station 'Wirtheim (list 'SE) 7.5) (make-station 'HaitzHoechst (list 'SE) 10.5))
 (define (distance-table-offset stations offset)
-  (define newOffset (+ offset (station-distance-to-next (first stations))))
   (cond
     [(empty? stations) empty]
-    [(empty? (rest stations)) (distance-table-offset (first stations) offset)]
-    [(not (empty? (rest stations))) (cons (make-station (station-name (first stations)) (station-train-kinds (first stations)) newOffset)  (distance-table-offset (rest stations) offset) )]
+    [(empty? (rest stations)) (list (make-station (station-name (first stations)) (station-train-kinds (first stations)) (+ offset (station-distance-to-next (first stations)))))]
+    [else (cons (make-station (station-name (first stations)) (station-train-kinds (first stations)) (+ offset (station-distance-to-next (first stations))))  (distance-table-offset (rest stations) (+ offset (station-distance-to-next (first stations)))) )]
   )
 )
 
-;; Tests (you have to provide at least two different tests)
+;; Tests
+(check-expect (distance-table (list
+                                 (make-station 'Waechtersbach '(RE SE) 4.5)
+                                 (make-station 'Wirtheim '(SE) 3)
+                               ) 'Waechtersbach)
+              (list (make-station 'Wirtheim (list 'SE) 3))
+)
+
+(check-expect (distance-table (list
+                                 (make-station 'Waechtersbach '(RE SE) 4.5)
+                               ) 'Waechtersbach)
+              empty
+)
 
 
-;; distance-table: 
-;; 
-;; Example: 
+;; distance-table: (listof stations) symbol -> (listof stations)
+;; Calculates the distance from the start station to all following stations
+;; Example: (distance-table (list
+;;            (make-station 'Waechtersbach '(RE SE) 4.5)
+;;            (make-station 'Wirtheim '(SE) 3)
+;;            (make-station 'HaitzHoechst '(SE) 3)
+;;          ) 'Waechtersbach 
+;;       =  (list (make-station 'Wirtheim (list 'SE) 3) (make-station 'HaitzHoechst (list 'SE) 6))
 
 
 (define (distance-table stations from-station)
   (cond
-    [(?empty stations) empty]
+    [(empty? stations) empty]
     [(eq? (station-name (first stations)) from-station) (distance-table-offset (rest stations) 0)]
+    [else (distance-table (rest stations) from-station)]
   )
-  (distance-table (rest stations) from-station)
+  
 )
 
-;; Tests (you have to provide at least two different tests)
-;; The following test is provided by us to help you.
-;; It does NOT count as one of the two mandatory tests!
+;; Test
 
-;; TODO Uncomment this
-;;(check-expect (distance-table (list (make-station 'AStadt '(IC SE) 0)
-;;                    (make-station 'BDorf '(SE) 2.5)
-;;                    (make-station 'CStadt '(IC SE) 8.5)
-;;              ) 'AStadt)
-;;              
-;;)
+(check-expect (distance-table-offset (list (make-station 'BDorf '(SE) 2.5)
+                                           (make-station 'CStadt '(IC SE) 8.5)
+                                     )
+               0
+              )
+              (list (make-station 'BDorf (list 'SE) 2.5) (make-station 'CStadt (list 'IC 'SE) 11)) 
+)
+
+(check-expect (distance-table-offset (list (make-station 'BDorf '(SE) 2.5) )
+               0
+              )
+              (list (make-station 'BDorf (list 'SE) 2.5) ) 
+)
+

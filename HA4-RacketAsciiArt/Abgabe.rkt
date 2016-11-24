f71feef3cc90ce67b3ebb0a5fb9f628523b0fae6
Paul Wagner <pw76kate@rbg.informatik.tu-darmstadt.de>
2016-11-24 23:30:17 +0100

    Added early version of 6.4

--- HA4-RacketAsciiArt/Abgabe.rkt
+++ HA4-RacketAsciiArt/Abgabe.rkt
@@ -244,11 +244,31 @@
 ;;
 ;; Example: (downscale (average3 (list (make-color 1 1 1) (make-color 2 2 2) (make-color 3 3 3) (make-color 4 4 4) 1)))
 ;; = (list (make-color 1 1 1))
-(define (downscale image width)
-  ; 1. Count all elements and pair them with the value
-  ; 2. Remove all odd rows
-  ; 3. Remove all odd elements in a row
-  ...)
+(define (downscale image image-width)
+        (local [(define (cut image image-width current-column crop-column crop-row)
+                        (cond [(empty? image) empty]
+                              [(= current-column 0) (cut image image-width image-width false (not crop-row))]
+                              [crop-row (cut (rest image) image-width (- current-column 1) crop-column crop-row)]
+                              [else (if crop-column
+                                        (cut (rest image)
+                                             image-width
+                                             (- current-column 1)
+                                             (not crop-column)
+                                             crop-row)
+                                        (cons (first image)
+                                              (cut (rest image)
+                                                   image-width
+                                                   (- current-column 1)
+                                                   (not crop-column)
+                                                   crop-row))
+                                    )
+                              ]
+                        )
+               )]
+          
+               (cut image image-width image-width false false)
+        )
+)
 
 ;; No tests necessary here
 
@@ -257,64 +277,65 @@
 ;; ====== Provided code ======
 
 ; Commented out because they depend on unimplemented procedures from above.
-;;; downsample: (listof color) integer -> (listof color)
-;;; Downsamples an image by a factor of 2, i.e. first blurs the image with horizontal kernel
-;;; 0.25 * [1 2 1], then removes every second pixel horizontally and every second row, pads with (0, 0, 0);
-;;; Example: (downsample (list (make-color 0 0 0) (make-color 1 1 1) (make-color 2 2 2) (make-color 3 3 3) 
-;;;                            (make-color 4 5 5) (make-color 5 5 5) (make-color 6 6 6) (make-color 7 7 7)) 4) returns
-;;;          (list (make-color 0 0 0) (make-color 2 2 2)) with width of 2
-;(define (downsample rgb-list width)
-;  (if
-;   (empty? rgb-list)
-;   empty
-;   (downscale (average3 rgb-list) width)))
-;
-;;; Test
-;;; error here -> student did forget to check for an empty list
-;(check-expect (downsample empty 0) empty)
-;;; error here -> student assumes that list-length is greater than 1 or pads wrong
-;(check-expect (downsample (list (make-color 128 128 128)) 1) (list (make-color 64 64 64 255)))
-;;; error here -> student probably does not take the imge's width correctly into account
-;(check-expect (downsample (list
-;                           (make-color 0 0 0) (make-color 1 1 1) (make-color 2 2 2) (make-color 3 3 3)
-;                           (make-color 4 4 4) (make-color 5 5 5) (make-color 6 6 6) (make-color 7 7 7)) 4)
-;              (list (make-color 0 0 0) (make-color 2 2 2)))
+;; downsample: (listof color) integer -> (listof color)
+;; Downsamples an image by a factor of 2, i.e. first blurs the image with horizontal kernel
+;; 0.25 * [1 2 1], then removes every second pixel horizontally and every second row, pads with (0, 0, 0);
+;; Example: (downsample (list (make-color 0 0 0) (make-color 1 1 1) (make-color 2 2 2) (make-color 3 3 3) 
+;;                            (make-color 4 5 5) (make-color 5 5 5) (make-color 6 6 6) (make-color 7 7 7)) 4) returns
+;;          (list (make-color 0 0 0) (make-color 2 2 2)) with width of 2
+(define (downsample rgb-list width)
+  (if
+   (empty? rgb-list)
+   empty
+   (downscale (average3 rgb-list) width)))
+
+;; Test
+;; error here -> student did forget to check for an empty list
+(check-expect (downsample empty 0) empty)
+;; error here -> student assumes that list-length is greater than 1 or pads wrong
+(check-expect (downsample (list (make-color 128 128 128)) 1) (list (make-color 64 64 64 255)))
+;; error here -> student probably does not take the imge's width correctly into account
+(check-expect (downsample (list
+                           (make-color 0 0 0) (make-color 1 1 1) (make-color 2 2 2) (make-color 3 3 3)
+                           (make-color 4 4 4) (make-color 5 5 5) (make-color 6 6 6) (make-color 7 7 7)) 4)
+              (list (make-color 0 0 0) (make-color 2 2 2)))
               
 
-;;; downsample-pyramid: (listof color) integer integer -> (listof color)
-;;; Executes pyramidical downsampling (i.e. n times downscaling by factor two), the resulting width is width / 2^(n - 1)
-;;; Example: (visual)
-;(define (downsample-pyramid rgb-list width n)
-;  (if
-;   (= n 0)
-;   ;; no more steps for downscaling - return image
-;   rgb-list
-;   ;; otherwise, sample one more step
-;   (downsample (downsample-pyramid rgb-list width (- n 1)) (/ width (expt 2 (- n 1))))))
-;
+;; downsample-pyramid: (listof color) integer integer -> (listof color)
+;; Executes pyramidical downsampling (i.e. n times downscaling by factor two), the resulting width is width / 2^(n - 1)
+;; Example: (visual)
+(define (downsample-pyramid rgb-list width n)
+  (if
+   (= n 0)
+   ;; no more steps for downscaling - return image
+   rgb-list
+   ;; otherwise, sample one more step
+   (downsample (downsample-pyramid rgb-list width (- n 1)) (/ width (expt 2 (- n 1))))))
+
 ;; here: only visual tests (load, downsample/average3 and export image, do a visual comparison)
 ;; load-averag3-save: string string integer -> boolean (and PNG-image on disk)
-;(define (load-average3-save in-path out-path)
-;  (local
-;    ((define in-img (bitmap/file in-path)))
-;    (save-image
-;     (color-list->bitmap
-;      (average3 (image->color-list in-img))
-;      (image-width in-img)
-;      (image-height in-img))
-;     out-path)))
-;
-;;; load-downscale-save: string string integer -> boolean (and PNG-image on disk)
-;(define (load-downscale-save in-path out-path)
-;  (local
-;    ((define in-img (bitmap/file in-path)))
-;    (save-image
-;     (color-list->bitmap
-;      (downscale (image->color-list in-img) (image-width in-img))
-;      (/ (image-width in-img) 2)
-;      (/ (image-height in-img) 2))
-;     out-path)))
-;
+(define (load-average3-save in-path out-path)
+  (local
+    ((define in-img (bitmap/file in-path)))
+    (save-image
+     (color-list->bitmap
+      (average3 (image->color-list in-img))
+      (image-width in-img)
+      (image-height in-img))
+     out-path)))
+
+;; load-downscale-save: string string integer -> boolean (and PNG-image on disk)
+(define (load-downscale-save in-path out-path)
+  (local
+    ((define in-img (bitmap/file in-path)))
+    (save-image
+     (color-list->bitmap
+      (downscale (image->color-list in-img) (image-width in-img))
+      (/ (image-width in-img) 2)
+      (/ (image-height in-img) 2))
+     out-path)))
+
+
 ;;; Visual tests (Only check for successful save)
 ;(check-expect (load-average3-save "fop-u04-material/gcc_logo.jpg" "fop-u04-material/gcc_logo_blurred.png") true)
 ;(check-expect (load-average3-save "fop-u04-material/solid_snake.jpg" "fop-u04-material/solid_snake_blurred.png") true)

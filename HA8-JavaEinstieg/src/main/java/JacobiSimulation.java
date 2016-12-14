b118e1280c8c01afc7fb9c15f54c88e108870d92
games647 <games@tsprivat.de>
2016-12-14 17:09:09 +0100

    Add the project related settings

---
+++ HA8-JavaEinstieg/src/main/java/JacobiSimulation.java
@@ -0,0 +1,26 @@
+//import acm.program.ConsoleProgram;
+//
+///**
+// *
+// *
+// *
+// */
+//public class JacobiSimulation extends ConsoleProgram {
+//  /**
+//   * available characters for display
+//   */
+//  static char[] chars = { '\u2003', '\u2581', '\u2582', '\u2583', '\u2584',
+//                   '\u2585', '\u2586', '\u2587', '\u2588' };
+//
+//  //TODO insert your code here!
+//
+//  /**
+//   * Starts the program
+//   *
+//   * @param args
+//   *            not used
+//   */
+//  public static void main(String args[]) {
+//    new JacobiSimulation(args).start();
+//  }
+//}

7c920552f16f7c5e6fd6d74fd1dc47937daf9785
games647 <games@tsprivat.de>
2017-01-01 19:12:28 +0100

    Add simple helper method for quering the column around the current index

--- HA8-JavaEinstieg/src/main/java/JacobiSimulation.java
+++ HA8-JavaEinstieg/src/main/java/JacobiSimulation.java
@@ -51,27 +51,55 @@ public class JacobiSimulation extends ConsoleProgram {
     }
 
     private void simulate() {
-        //reset the working state
+        //reset the working state of difference and jacobi maps
         difference = 0;
+
         //just move the reference, but we have to create a new current
         //so it wouldn't be the same object as the previous object
         previous = current;
         current = new double[height][width];
 
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                double newVal = 0.25 * (j > 0 ? previous[i][j - 1] : 1.0)
-                        + (j < height - 1 ? previous[i][j + 1] : 1.0)
-                        + (i > 0 ? previous[i - 1][j] : 1.0)
-                        + (i < width - 1 ? previous[i + 1][j] : 1.0);
+        for (int posY = 0; posY < height; posY++) {
+            for (int posX = 0; posX < width; posX++) {
+                double left = getOrDefault(previous, posY, posX - 1, 1);
+                double right = getOrDefault(previous, posY, posX + 1, 1);
 
-                current[i][j] = newVal;
+                double above = getOrDefault(previous, posY - 1, posX, 1);
+                double below = getOrDefault(previous, posY + 1, posX, 1);
+
+                double newVal = 0.25 * (left + right + above + below);
+                current[posY][posX] = newVal;
 
                 //the difference should be always positive, so calculate the absolute value
-                double previousVal = previous[i][j];
+                double previousVal = previous[posY][posX];
                 difference += Math.abs(newVal - previousVal);
             }
         }
+    }
+
+    /**
+     * Gets an element of the two dimensional array or return the given default value if it's outside
+     * of the array.
+     *
+     * @param array the array to be searched in
+     * @param posX  index of the second dimension
+     * @param posY  index of the first dimension
+     * @param def   default value if it's outside the array
+     * @return the array element or the default value
+     */
+    private double getOrDefault(double[][] array, int posY, int posX, double def) {
+        //the first dimension is out of range
+        if (posY < 0 || posY >= array.length) {
+            return def;
+        }
+
+        //second dimension is out of range
+        double[] val = array[posY];
+        if (posX < 0 || posX >= val.length) {
+            return def;
+        }
+
+        return val[posX];
     }
 
     /**

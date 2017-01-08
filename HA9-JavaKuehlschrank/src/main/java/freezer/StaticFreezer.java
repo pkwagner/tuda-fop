27151ccb30f6f970123a0aff9bb2aec457a09cad
games647 <games@tsprivat.de>
2017-01-08 17:22:43 +0100

    Add StaticFreezer class from given tasks

---
+++ HA9-JavaKuehlschrank/src/main/java/freezer/StaticFreezer.java
@@ -0,0 +1,29 @@
+package freezer;
+
+/**
+ * Static class to define available Freezer sizes
+ *
+ * @author Nicolas Weber
+ */
+class StaticFreezer {
+
+    /**
+     * Contains all available
+     */
+    final static double[] availableWidths = new double[]{0.5, 1.0};
+
+    /**
+     *
+     */
+    final static double[] availableHeights = new double[]{1.0, 2.0};
+
+    /**
+     *
+     */
+    final static double[] availableDepths = new double[]{0.5, 1.0, 1.5};
+
+    /**
+     *
+     */
+    final static double[] availableWallThickness = new double[]{0.01, 0.1};
+}

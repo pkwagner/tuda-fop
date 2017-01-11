fbf1e4fcaf4ce55bf16a2f072dba226f29ed5290
games647 <games@tsprivat.de>
2017-01-11 19:22:13 +0100

    Add custom unit tests

---
+++ HA9-JavaKuehlschrank/src/test/java/freezer/EnergyClassTest.java
@@ -0,0 +1,30 @@
+package freezer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Energy class tests
+ *
+ * @author Alexander Siegler
+ * @author Paul Konstantin Wagner
+ * @author Yoshua Hitzel
+ * @author Marcel Lackovic
+ */
+public class EnergyClassTest {
+
+    @Test
+    public void getEnergyClassTest() throws Exception {
+        //lowest
+        assertEquals(EnergyClass.D, EnergyClass.getEnergyClass(0));
+
+        //between two entries
+        assertEquals(EnergyClass.A, EnergyClass.getEnergyClass(0.15));
+
+        //highest class
+        assertEquals(EnergyClass.Appp, EnergyClass.getEnergyClass(0.2));
+        //a lot higher
+        assertEquals(EnergyClass.Appp, EnergyClass.getEnergyClass(0.5));
+    }
+}

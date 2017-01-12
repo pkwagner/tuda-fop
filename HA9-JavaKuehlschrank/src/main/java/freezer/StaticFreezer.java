3d947c9d0d76527497e0c4778fa6ffe68c86706a
games647 <games@tsprivat.de>
2017-01-12 13:52:38 +0100

    Document static freezer components

--- HA9-JavaKuehlschrank/src/main/java/freezer/StaticFreezer.java
+++ HA9-JavaKuehlschrank/src/main/java/freezer/StaticFreezer.java
@@ -8,22 +8,22 @@ package freezer;
 class StaticFreezer {
 
     /**
-     * Contains all available
+     * Contains all available widths of a freezer configurations
      */
     final static double[] availableWidths = new double[]{0.5, 1.0};
 
     /**
-     *
+     * Contains all available heights configurations
      */
     final static double[] availableHeights = new double[]{1.0, 2.0};
 
     /**
-     *
+     * Contains all available depths configurations
      */
     final static double[] availableDepths = new double[]{0.5, 1.0, 1.5};
 
     /**
-     *
+     * Contains all available wall thickness configurations
      */
     final static double[] availableWallThickness = new double[]{0.01, 0.1};
 }

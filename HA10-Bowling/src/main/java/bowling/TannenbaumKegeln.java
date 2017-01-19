55f5603ad4adb294242cb3e3c0f4ee668391901c
Konstantin Wagner <git@alphath.space>
2017-01-19 19:59:00 +0100

    Updated class TannenbaumKegeln to match the new parent class implementation

--- HA10-Bowling/src/main/java/bowling/TannenbaumKegeln.java
+++ HA10-Bowling/src/main/java/bowling/TannenbaumKegeln.java
@@ -1,5 +1,6 @@
 package bowling;
 
+import java.util.Arrays;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
@@ -12,10 +13,6 @@ import java.util.stream.Stream;
  * @author Marcel Lackovic
  */
 public class TannenbaumKegeln extends Game {
-    static final int maxThrows = 2;
-
-    FirTreeMap firTreeMaps[];
-
     /**
      * Creates a new game instance with a game mode called 'Tannenbaum-Kegeln'
      *
@@ -26,32 +23,54 @@ public class TannenbaumKegeln extends Game {
 
         this.maxRounds = 100;
         this.maxPins = 9;
+        this.maxThrows = 2;
         this.gameMode = "Tannenbaum-Kegeln";
-
-        this.firTreeMaps = new FirTreeMap[maxPlayer];
-        for (int i = 0; i < firTreeMaps.length; i++)
-            firTreeMaps[i] = new FirTreeMap(new int[]{0, 1, 2, 7, 6, 5, 4, 3, 2, 1});
     }
 
     @Override
-    public boolean throwBall(int count) {
-        if (!super.throwBall(count))
-            return false;
+    public boolean startGame() {
+        if (super.startGame()) {
+            this.scores = new int[this.activePlayersCounter][10];
+            for (int i = 0; i < this.scores.length; i++)
+                this.scores[i] = new int[]{0, 1, 2, 7, 6, 5, 4, 3, 2, 1};
 
-        // Was this the last round or did the player achieve a strike?
-        // NOTE: pinsLeft was used instead of maxPins to include spares
-        if ((this.getThrow() > TannenbaumKegeln.maxThrows) || (count == this.pinsLeft)) {
-            this.firTreeMaps[this.getActivePlayer().getID()].subtractFromGoal(this.getThrownPins());
-
-            if (this.checkFirTree(this.getActivePlayer()))
-                this.finishGame(this.getActivePlayer());
-
-            if (!this.nextPlayer())
-                this.finishGame(getPlayerWithBestFirTree());
+            return true;
         }
 
-        return true;
+        return false;
     }
+
+    @Override
+    protected void onThrow(int count) {
+        if (this.getThrow() >= this.getMaxThrows(count)) {
+            this.scores[this.getActivePlayer().getID()][this.getThrownPins()] -= (this.scores[this.getActivePlayer().getID()][this.getThrownPins()] > 0) ? 1 : 0;
+
+            if (this.checkFirTree(this.getActivePlayer())) {
+                // Set current player as winner
+                this.finished = true;
+                this.winner = this.getActivePlayer();
+            }
+        }
+    }
+
+    @Override
+    protected int getMaxThrows(int count) {
+        // Was this the last round or did the player achieve a strike?
+        // NOTE: pinsLeft was used instead of maxPins to include spares
+        if (this.pinsLeft == 0)
+            return 0;
+        else
+            return super.getMaxThrows(count);
+    }
+
+    @Override
+    public Player getWinner() {
+        if (this.winner == null) {
+            return getPlayerWithBestFirTree();
+        } else
+            return this.winner;
+    }
+
 
     /**
      * Checks if a given user's firTree is empty (game aim)
@@ -60,32 +79,20 @@ public class TannenbaumKegeln extends Game {
      * @return true if the firTree is empty, false if there are still pending goals
      */
     private boolean checkFirTree(Player player) {
-        return Stream.of(firTreeMaps[player.getID()]).allMatch(Predicate.isEqual(0));
+        return Stream.of(scores[player.getID()]).allMatch(Predicate.isEqual(0));
     }
 
     private Player getPlayerWithBestFirTree() {
-        int firTreeResults[] = Stream.of(firTreeMaps)
-                .mapToInt(FirTreeMap::getRemainingGoalsAmount)
-                .toArray();
-
         int bestPlayerId = 0, bestPlayerScore = -1;
-        for (int i = 0; i < firTreeResults.length; i++) {
-            if ((firTreeResults[i] < bestPlayerScore) || (bestPlayerScore == -1)) {
+        for (int i = 0; i < scores.length; i++) {
+            int playerScore = Arrays.stream(scores[i]).sum();
+
+            if ((playerScore < bestPlayerScore) || (bestPlayerScore == -1)) {
                 bestPlayerId = i;
-                bestPlayerScore = firTreeResults[i];
+                bestPlayerScore = playerScore;
             }
         }
 
         return this.getPlayer(bestPlayerId);
-    }
-
-    /**
-     * Should be called if there is a winner. The game will be finished and a given player will be set as winner.
-     *
-     * @param winner the player who won the game
-     */
-    private void finishGame(Player winner) {
-        this.finished = true;
-        this.winner = winner;
     }
 }
